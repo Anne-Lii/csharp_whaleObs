0a7196b01769f33fa9d6109611867426d79c47cb
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-10-17 07:53:42 +0200

    skapat en meny och en switch-sats för de olika valen där metoder anropas. Skapat en fil Observation.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Anne-Lii Hansen anha2324@student.miun.se
+/*
+En app för valobservationer där användaren kan registrera en observation av en val.
+*/
+
+using System;
+using System.Diagnostics;
+
+namespace whaleObservationApp
+{
+    class Program
+    {
+        static List<> observationer = new List<>();
+        static void Main(string[] args)
+        {
+            bool programRunning = true; //variabel med true false 
+
+            while (programRunning)
+            {
+                Console.Clear();//Rensar konsollen vid varje menyval
+                Console.WriteLine("Välkommen till appen för valobservationer!");
+                Console.WriteLine("Välj ett alternativ (1-4)");
+                Console.WriteLine("1. Logga en valobservation");
+                Console.WriteLine("2. Visa alla valobservationer");
+                Console.WriteLine("3. Ta bort valobservation");
+                Console.WriteLine("4. Avsluta");
+
+                string? input = Console.ReadLine();
+
+               switch (input)
+               {
+                case "1":
+                LoggObservation();//anropar metod för att logga observation
+                break;
+
+                case "2":
+                ShowObservation();//anropar metod för att lista observationer
+                break;
+
+                case "3":
+                RemoveObservation();//anropar metod för att ta bort observation
+                break;
+
+                case "4":
+                programRunning = false; //avslutar programmet
+                break;
+
+                default:
+                Console.WriteLine("Ogiltigt val, försök igen.");//felmeddelande
+                Console.ReadKey();//väntar på att användaren trycker på en tangent
+                break;
+               }
+            }
+        }
+    }
+}
